9c57bab73439bc2a88269ed759d599ab3ecae82a
Ahmad Tsaqif <ahmadtsaqif007@gmail.com>
2023-11-23 11:53:35 +0700

    added image buffer too show the character sprite

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -9,10 +9,11 @@ public class GameWindow {
 
 		jframe = new JFrame();
 
-		jframe.setSize(690, 690);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.add(gamePanel);
 		jframe.setLocationRelativeTo(null);
+		jframe.setResizable(false);
+		jframe.pack();
 		jframe.setVisible(true);
 
 	}
